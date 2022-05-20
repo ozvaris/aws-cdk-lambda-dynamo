363d8db9d9e5af5c9b37eba4749ba0570652cbe0
Ozgur <ozgurozvaris@hotmail.com>
2022-05-20 17:13:09 +0100

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import { Configuration } from "webpack";
+import { resolve } from "path";
+
+const config: Configuration = {
+  mode: "none",
+  entry: {
+    nodeHelloLambda: "./services/node-lambda/hello.ts",
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            // pass special tsconfig file
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    libraryTarget: "commonjs2",
+    path: resolve(__dirname, "build"),
+    filename: "[name]:[name].js",
+  },
+};
+
+export default config;
